c4875c46d8efcb9bf4c8602017b48de611f2771a
ToveLissner <tove_lissner@hotmail.com>
2022-12-12 11:26:40 +0100

    Produkter

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,12 +1,13 @@
 import { Product } from "./models/Product";
 
 /* Headcovers */
+
 let headcover1: Product = new Product(
   "Headcover Gris",
   "499",
   "Headcovers som passar för din driver. Finns i en mängd olika motiv så att du kan finna just din favorit.",
   "Headcovers",
-  "../assets/products/Headcover_1.jpg", // har jag tänkt fel här???
+  "../assets/products/Headcover_1.jpg",
   "headcover1"
 );
 
@@ -15,7 +16,7 @@ let headcover2: Product = new Product(
   "499",
   "Headcovers som passar för din driver. Finns i en mängd olika motiv så att du kan finna just din favorit.",
   "Headcovers",
-  "../assets/products/Headcover_2.jpg", // har jag tänkt fel här???
+  "../assets/products/Headcover_2.jpg",
   "headcover2"
 );
 
@@ -24,7 +25,7 @@ let headcover3: Product = new Product(
   "499",
   "Headcovers som passar för din driver. Finns i en mängd olika motiv så att du kan finna just din favorit.",
   "Headcovers",
-  "..assets/products/Headcover_3.jpg", // har jag tänkt fel här???
+  "..assets/products/Headcover_3.jpg",
   "headcover3"
 );
 
@@ -37,7 +38,7 @@ let proFliHi: Product = new Product(
   "2799",
   "Mizuno Pro har alltid varit Mizunos mest innovationsinriktade serie med avancerad ingenjörskonst dold i ett stilrent och tidlöst paket. Mizuno Pro är järnklubbor för så väl proffsgolfare som amatörer som värdesätter mjuk träffkänsla och elegant design.",
   "Golfklubbor",
-  "../assets/products/Pro_Fli_Hi.jpg", // har jag tänkt fel här???
+  "../assets/products/Pro_Fli_Hi.jpg",
   "proFliHi"
 );
 
@@ -46,7 +47,7 @@ let stealthUDI: Product = new Product(
   "2899",
   "Stealth UDI är ett tourinspirerat utilityjärn riktat mot den bättre spelaren. Tyngdpunkten är placerad för att skapa en låg och penetrerande bollflykt med maximal kontroll.",
   "Golfklubbor",
-  "../assets/products/Stealth_UDI.png", // har jag tänkt fel här???
+  "../assets/products/Stealth_UDI.png",
   "stealthUDI"
 );
 
@@ -55,7 +56,7 @@ let u510: Product = new Product(
   "2699",
   "Titleist U510 är ett utility-järn med hybridliknande egenskaper men med formen av en kraftfull järnklubba för högre utgångsvinkel och längd.",
   "Golfklubbor",
-  "../assets/products/U510.jpg", // har jag tänkt fel här???
+  "../assets/products/U510.jpg",
   "u510"
 );
 
@@ -70,7 +71,7 @@ let tp5Pop: Product = new Product(
   "599",
   "Golfboll med...",
   "Golfbollar",
-  "../assets/products/TP5_PIX_ROCKET_POP.jpg", // har jag tänkt fel här???
+  "../assets/products/TP5_PIX_ROCKET_POP.jpg",
   "tp5Pop"
 );
 
@@ -81,7 +82,7 @@ let tp5Sushi: Product = new Product(
   "599",
   "Golfboll med fräsch doft av fisk",
   "Golfbollar",
-  "../assets/products/TP5_PIX_SUSHI.jpg", // har jag tänkt fel här???
+  "../assets/products/TP5_PIX_SUSHI.jpg",
   "tp5Sushi"
 );
 
@@ -92,7 +93,7 @@ let tp5Spider: Product = new Product(
   "599",
   "Perfekt för familj med pälsallergi!",
   "Golfbollar",
-  "../assets/products/TP5_PIX_SPIDER.jpg", // har jag tänkt fel här???
+  "../assets/products/TP5_PIX_SPIDER.jpg",
   "tp5Spider"
 );
 
@@ -118,30 +119,34 @@ function createHTMLForProduct(productList: Product[]) {
   ) as HTMLDivElement;
 
   for (let i: number = 0; i < productList.length; i++) {
+    let productDiv: HTMLDivElement = document.createElement("div");
     let productContainer: HTMLDivElement = document.createElement("div");
     let title: HTMLHeadingElement = document.createElement("h3");
-    let price: HTMLParagraphElement = document.createElement("p");
     let imageContainer: HTMLDivElement = document.createElement("div");
     let image: HTMLImageElement = document.createElement("img");
+    let price: HTMLParagraphElement = document.createElement("p");
     let description: HTMLParagraphElement = document.createElement("p");
 
-    productContainer.id = "productContainer";
-    price.id = "price";
-    imageContainer.id = "imageContainer";
-    image.className = "img";
-    image.id = productList[i].id;
+    productDiv.className = "product";
+    productContainer.className = "product" + "__" + productList[i].id;
+    title.className = "product__title";
+    imageContainer.className = "product__imageContainer";
+    image.className = "product__imageContainer__img";
+    price.className = "product__price";
+    description.className = "product__description";
 
     title.innerHTML = productList[i].title;
     price.innerHTML = productList[i].price; // måste göra om till number sen
     description.innerHTML = productList[i].description;
 
     productContainer.appendChild(title);
-    productContainer.appendChild(price);
-    productContainer.appendChild(description);
     productContainer.appendChild(imageContainer);
     imageContainer.appendChild(image);
+    productContainer.appendChild(price);
+    productContainer.appendChild(description);
 
-    bigContainer.appendChild(productContainer);
+    productDiv.appendChild(productContainer);
+    bigContainer.appendChild(productDiv);
   }
 }
 

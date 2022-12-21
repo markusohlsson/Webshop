85d78fba10276077a1d559be2044c4d214a430bb
ToveLissner <tove_lissner@hotmail.com>
2022-12-21 14:07:41 +0100

    styling i varukorgen

--- src/ts/cart.ts
+++ src/ts/cart.ts
@@ -4,75 +4,80 @@ import { productList } from "./services/productService";
 // Hamburger
 
 hamburgericon?.addEventListener("click", function (event) {
-    hamburgermenu();
-  });
-  
+  hamburgermenu();
+});
 
+// Products
 
-  // Products 
+let shoppingCartContainer: HTMLDivElement = document.getElementById(
+  "shoppingCartContainer"
+) as HTMLDivElement;
+let cartButton: HTMLButtonElement = document.createElement("button");
+let title: HTMLHeadingElement = document.createElement(
+  "h3"
+) as HTMLHeadingElement;
+title.innerHTML = "Varukorg";
+shoppingCartContainer.appendChild(title);
+let aTag: HTMLAnchorElement = document.createElement("a");
+aTag.href = "../pages/checkout.html";
+aTag.appendChild(cartButton);
 
-  let shoppingCartContainer: HTMLDivElement = document.getElementById("shoppingCartContainer") as HTMLDivElement;
-  let cartButton: HTMLButtonElement = document.createElement("button");
-  let title:HTMLHeadingElement = document.createElement("h3") as HTMLHeadingElement;
-  title.innerHTML="Varukorg";
-  shoppingCartContainer.appendChild(title);
-  let aTag:HTMLAnchorElement = document.createElement("a");
-  aTag.href="../pages/checkout.html";
-  aTag.appendChild(cartButton);
-function test(){
-  var retrievedObject = JSON.parse(localStorage.getItem("cartList")||"");
+// tror inte att vi får använda "var" //
+
+function test() {
+  var retrievedObject = JSON.parse(localStorage.getItem("cartList") || "");
   let nylista = [];
   for (let i = 0; i < retrievedObject.length; i++) {
     nylista.push(retrievedObject);
     console.log(retrievedObject);
   }
-  
-  for (let i = 0; i < nylista.length; i++) {
 
-    let productDiv: HTMLDivElement = document.createElement("div");
+  // OBS får vi ha svenskt namn här (en lista) ? //
+
+  for (let i = 0; i < nylista.length; i++) {
+    // let productDiv: HTMLDivElement = document.createElement("div");
     let productContainer = document.createElement("div");
     let title: HTMLHeadingElement = document.createElement("h3");
     let imageContainer: HTMLDivElement = document.createElement("div");
     let image: HTMLImageElement = document.createElement("img");
     let price: HTMLParagraphElement = document.createElement("p");
-    let description: HTMLParagraphElement = document.createElement("p");
+    // let description: HTMLParagraphElement = document.createElement("p");
     let amountInput: HTMLInputElement = document.createElement("input");
 
-
-
-
-    productDiv.className = "product";
+    // productDiv.className = "product";
     productContainer.className = "product" + "__" + retrievedObject[i].id;
+    productContainer.id = "pCId";
     title.className = "product__title";
     imageContainer.className = "product__imageContainer";
     image.className = "product__imageContainer__img";
     price.className = "product__price";
-    description.className = "product__description";
+    // description.className = "product__description";
+    amountInput.className = "product__amountInput";
     amountInput.type = "number";
-    amountInput.value=retrievedObject[i].amount;
-    cartButton.innerHTML="Gå till kassan";
-  
+    amountInput.value = retrievedObject[i].amount;
+    cartButton.innerHTML = "Gå till kassan";
+
     // let amount:number = amountInput.valueAsNumber;
-  
+
     title.innerHTML = retrievedObject[i].title;
     price.innerHTML = retrievedObject[i].price + " " + "kr"; // måste göra om till number sen
-    description.innerHTML = retrievedObject[i].description;
-  
-    productContainer.appendChild(title);
+    // description.innerHTML = retrievedObject[i].description;
+
     productContainer.appendChild(imageContainer);
     imageContainer.appendChild(image);
-    productContainer.appendChild(description);
+    productContainer.appendChild(title);
+    // productContainer.appendChild(description);
     productContainer.appendChild(price);
     productContainer.appendChild(amountInput);
-    productDiv.appendChild(productContainer);
-    shoppingCartContainer.appendChild(productDiv);
+    // productDiv.appendChild(productContainer);
+    shoppingCartContainer.appendChild(productContainer);
 
-    amountInput.addEventListener("input", ()=>{
+    amountInput.addEventListener("input", () => {
       if (retrievedObject[i].amount < amountInput.value) {
         retrievedObject[i].amount++;
-    } else {
-      retrievedObject[i].amount--;
-    }
+      } else {
+        retrievedObject[i].amount--;
+      }
       localStorage.setItem("cartList", JSON.stringify(retrievedObject));
       console.log(retrievedObject[i].amount);
     });
